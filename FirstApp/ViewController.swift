280382903c0f5cd17d31e9c07a15b066155a0bb0
Oguzhan Alpayli <oguzhan.alpayli@tofnatech.com>
2018-10-08 14:22:59 +0300

    Initial Commit

---
+++ FirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by Oguzhan Alpayli on 8.10.2018.
+//  Copyright © 2018 Oguzhan Alpayli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
